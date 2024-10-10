8bfd3970fc8e66df67276bb603ac26ff56d15cd4
sarapolajnar <sp16583@student.uni-lj.si>
2024-10-10 14:49:27 +0200

    popravek kode z while

--- pridobivanje_podatkov/podatki.py
+++ pridobivanje_podatkov/podatki.py
@@ -6,46 +6,55 @@ import pandas as pd
 
 podatki = []
 
-for i in range(1, 93): 
-    url = (f"https://worldathletics.org/records/toplists/sprints/60-metres/all/women/senior/2024?"
-            f"regionType=world&timing=electronic&windReading=regular&page={i}&bestResultsOnly=true&"
-            f"maxResultsByCountry=all&eventId=10229684&ageCategory=senior")
+i = 1
 
-    odziv = requests.get(url)
+while True:
+    try:
+        url = (f"https://worldathletics.org/records/toplists/sprints/60-metres/all/women/senior/2024?"
+                f"regionType=world&timing=electronic&windReading=regular&page={i}&bestResultsOnly=true&"
+                f"maxResultsByCountry=all&eventId=10229684&ageCategory=senior")
 
-    if odziv.status_code != 200:
-        print("Prišlo je do napake")
-        continue 
+        odziv = requests.get(url)
 
-    soup = BeautifulSoup(odziv.text, 'html.parser')
+        if odziv.status_code != 200:
+            print("Prišlo je do napake")
+            continue 
 
-    tabela = soup.find('table')
+        soup = BeautifulSoup(odziv.text, 'html.parser')
 
-    # Preberi vrstice iz tabele
-    for vrstica in tabela.find_all('tr')[1:]:  # preskočimo prvo vrstico (glave), poiscemo tr
-        celica = vrstica.find_all('td')         #vsi podatki se nahajajo med td
+        tabela = soup.find('table')
 
-        if celica:
-            rank = celica[0].text.strip()   
-            wind = celica[2].text.strip()  
-            competitor_link = celica[3].find('a')    #ime tekmovalke je za linkom med a-ji
-            competitor = competitor_link.text.strip() if competitor_link else 'N/A'
-            dob = celica[4].text.strip()  
-            country = celica[5].text.strip()
-            position = celica[6].text.strip()         
-            date = celica[9].text.strip()   
-            
-            #naredimo slovar
-            podatki_tekmovalke = {
-                'uvrstitev': rank,
-                'veter': wind,
-                'ime tekmovalke': competitor,
-                'datum rojstva': dob,
-                'država': country,
-                'pozicija' : position,
-                'datum dogodka': date,
-            }
-            podatki.append(podatki_tekmovalke)
+        # Preberi vrstice iz tabele
+        for vrstica in tabela.find_all('tr')[1:]:  # preskočimo prvo vrstico (glave), poiscemo tr
+            celica = vrstica.find_all('td')         #vsi podatki se nahajajo med td
+
+            if celica:
+                rank = celica[0].text.strip()   
+                wind = celica[2].text.strip()  
+                competitor_link = celica[3].find('a')    #ime tekmovalke je za linkom med a-ji
+                competitor = competitor_link.text.strip() if competitor_link else 'N/A'
+                dob = celica[4].text.strip()  
+                country = celica[5].text.strip()
+                position = celica[6].text.strip()         
+                date = celica[9].text.strip()   
+                
+                #naredimo slovar
+                podatki_tekmovalke = {
+                    'uvrstitev': rank,
+                    'veter': wind,
+                    'ime tekmovalke': competitor,
+                    'datum rojstva': dob,
+                    'država': country,
+                    'pozicija' : position,
+                    'datum dogodka': date,
+                }
+                podatki.append(podatki_tekmovalke)
+
+        i += 1
+
+    except Exception as e:          #ce se zgodi napaka...npr tudi ko zmanjka strani se zanka ustavi
+        print(f"Napaka pri strani {i}: {e}")
+        break
 
 ime_mape = "pridobivanje_podatkov"
 
